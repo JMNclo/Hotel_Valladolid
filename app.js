f7530c4c940a60a07ac9ea1d5ad10207caf943c7
JMNclo <67565684+JMNclo@users.noreply.github.com>
2020-07-03 20:03:32 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+function kom() {
+	window.location="index.html";
+}
+
+function irface() {
+	window.location="https://www.facebook.com/hotelvalladolidhuajuapan";
+}
+
+function comen() {
+	alert("Gracias Por Tu Comentario! :)");
+}
+
